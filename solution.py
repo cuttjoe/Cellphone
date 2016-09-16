d93f19e9ca1086e17624293882ddf0c0f42f192d
DankNovaGaming <jesseehrenreich@gmail.com>
2016-09-16 09:20:18 +0200

    Jesse George Ehrenreich
    My CellPhone code!

--- solution.py
+++ solution.py
@@ -1,1 +1,42 @@
-#code here
+import time
+CellC=[]
+Voda=[]
+MTN=[]
+for i in range(0,10):
+	CellPhone=input("Enter a CellPhone")
+	if len(CellPhone)!=10:
+		print ("____")
+		print ("NOT A NUMBER")
+		print ("____")
+		time.sleep(5)
+		for i in range(0,100):
+			print ("")
+	if (CellPhone[:3])==("084") and len(CellPhone)==10:
+		print ("____")
+		print ("Cell C")
+		print ("____")
+		time.sleep(5)
+		CellC.append(CellPhone)
+		for i in range(0,100):
+			print ("")
+	if (CellPhone[:3])==("076") and len(CellPhone)==10:
+		print ("____")
+		print ("Voda")
+		print ("____")
+		time.sleep(5)
+		Voda.append(CellPhone)
+		for i in range(0,100):
+			print ("")
+	if (CellPhone[:3])==("083") and len(CellPhone)==10:
+		print ("____")
+		print ("MTN")
+		print ("____")
+		time.sleep(5)
+		MTN.append(CellPhone)
+		for i in range(0,100):
+			print ("")
+print ("Cell C Numbers:",CellC,"")
+print ("Voda Numbers:",Voda,"")
+print ("MTB Numbers:",MTN,"")
+		
+		
